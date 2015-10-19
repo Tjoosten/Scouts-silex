89ee3acc01497b418c3a5524f93df706658284f5
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-10-19 13:41:33 +0200

    Update frontpage.php

--- controllers/frontpage.php
+++ controllers/frontpage.php
@@ -4,8 +4,12 @@ use Symfony\Component\HttpFoundation\Request;
 
 $page = $app['controllers_factory'];
 
+/**
+ * 
+ */
 $page->get('/', function() use($app) {
-    $viewVariables['title'] = $app['translator']->trans('title_frontpage');
+    $viewVariables['title']  = $app['translator']->trans('title_frontpage');
+    $viewVariables['active'] = 0; // 0 because there is no statement for this active status. 
 
     return $app['twig']->render('frontpage.html.twig', $viewVariables);
 });
